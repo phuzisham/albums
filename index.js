bf17a6ae55b320b8ecfddeaa5785095ad40b69bb
David Hale <david.hale7@gmail.com>
2019-05-03 13:22:24 -0700

    first edit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+// Import a library to create component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create component
+const App = () => <Text>Some Text</Text>;
 
-AppRegistry.registerComponent(appName, () => App);
+// Render to device
+AppRegistry.registerComponent('albums', () => App);
