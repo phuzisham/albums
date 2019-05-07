27f9b1b29fa972c78f2abd648797885f48cdd0e8
David Hale <david.hale7@gmail.com>
2019-05-06 18:50:27 -0700

    add button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+    const { buttonStyle, textStyle } = styles;
     return (
-        <Text>Button!</Text>
+        <TouchableOpacity style={buttonStyle}>
+            <Text style={textStyle}>Click to buy!</Text>
+        </TouchableOpacity>
     );
+};
+
+const styles = {
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#5C8492',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#866161',
+        marginLeft: 5,
+        marginRight: 5
+    },
+    textStyle: {
+        alignSelf: 'center',
+        color: '#570000',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10 
+    }
 };
 
 export default Button;
